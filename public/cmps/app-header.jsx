58223185dd2d124bf90d98ce0d3b758bdeaa1905
Yarden Naor <naoryarden@gmail.com>
2023-01-04 21:11:54 +0200

    almost finished

--- public/cmps/app-header.jsx
+++ public/cmps/app-header.jsx
@@ -1,17 +1,32 @@
 const { NavLink } = ReactRouterDOM
-const {  useEffect } = React
+const {  useEffect, useState } = React
 
 import { UserMsg } from './user-msg.jsx'
 
+import { userService } from '../services/user.service.js'
+import { LoginSignup } from './login-signup.jsx'
+
 export function AppHeader() {
 
+  const [user,setUser]  = useState(userService.getLoggedinUser())
+
+  function onChangeLoginStatus(user){
+        setUser(user)
+    }
+  
+
+  function onLogout() {
+    userService.logout()
+        .then(()=>{
+            setUser(null)
+        })
+}
 
     useEffect(() => {
         // component did mount when dependancy array is empty
     }, [])
 
-    return (
-        <header>
+    return  <header>
             <UserMsg />
             <nav>
                 <NavLink to="/">Home</NavLink> |
@@ -19,6 +34,16 @@ export function AppHeader() {
                 <NavLink to="/about">About</NavLink>
             </nav>
             <h1>Bugs are Forever</h1>
+            {user ? (
+                < section >
+                    <h2>Hello {user.fullname}</h2>
+                    <button onClick={onLogout}>Logout</button>
+                </ section >
+            ) : (
+                <section>
+                    <LoginSignup onChangeLoginStatus={onChangeLoginStatus} />
+                </section>
+            )}
         </header>
-    )
+    
 }
